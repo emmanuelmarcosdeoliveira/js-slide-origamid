463042f72729d31993800557ae3d08e564114dd8
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-05-28 23:26:54 -0300

    style:finished styling
    reviewed: Emmanuel Oliveira

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+import "./src/script/script.js";
+import "./src/script/modules/slide.js";
+import "./src/styles/styles.css";
 document.querySelector("#app").innerHTML = `
-  <div>
-    <h1> Hello World</h1>
+  <div class="slide-wrapper">
+    <ul class="slide">
+       <li><img src="./foto1.jpg" alt="Raposa Vermelha " />
+       <li><img src="./foto2.jpg" alt="Raposa Vermelha " />
+       <li><img src="./foto3.jpg" alt="Raposa Vermelha " />
+       <li><img src="./foto4.jpg" alt="Raposa Vermelha " />
+       <li><img src="./foto5.jpg" alt="Raposa Vermelha " />
+       <li><img src="./foto6.jpg" alt="Raposa Vermelha " />
+   </ul>
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
