2a939f049ec1d2cb119b7926f31212a28469c514
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-05-30 14:05:33 -0300

    chore:stylized custom controls
    reviewed: Emmanuel Oliveira

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,5 @@
-import { SlideNav } from "./modules/slide.js";
+import SlideNav from "./modules/slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControl();
-console.log(slide);
+slide.addControl(".custom-controls");
